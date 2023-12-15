551e791d22473ab2ec1a6a986cb102983dd08def
xsankalp13 <sankalpbsalve@gmail.com>
2023-12-15 23:07:21 +0530

    feat: Implement NavItems component and routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
        <main>
-          <Button variant="destructive" className='px-10'> Delete </Button>
+          
        </main>
   )
 }
